73159420d1e0b23a049dc5a428190a7725965d9a
YuYu1015 <whes1015@gmail.com>
2024-12-11 13:55:02 +0800

    init: init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/your-repo-name' : '',
 };
 
 export default nextConfig;
